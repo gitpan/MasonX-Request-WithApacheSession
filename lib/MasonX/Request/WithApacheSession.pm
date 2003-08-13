03228c47a1286e2bd3515af1af36ab7a0c9ebdf4
Dave Rolsky <autarch@urth.org>
2003-08-13 01:08:21 +0000

    Import of DROLSKY/MasonX-Request-WithApacheSession-0.22 from CPAN.
    gitpan-cpan-distribution: MasonX-Request-WithApacheSession
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         DROLSKY/MasonX-Request-WithApacheSession-0.22.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/MasonX/Request/WithApacheSession.pm
+++ lib/MasonX/Request/WithApacheSession.pm
@@ -5,7 +5,7 @@ use strict;
 
 use vars qw($VERSION @ISA);
 
-$VERSION = '0.21';
+$VERSION = '0.22';
 
 use Apache::Session;
 
@@ -219,8 +219,8 @@ $ApacheSessionParams{Oracle} =
       $ApacheSessionParams{Postgres};
 
 my %OptionalApacheSessionParams =
-    ( Sybase => [ qw( textsize ) ],
-      Oracle => [ qw( long_read_len ) ],
+    ( Sybase => [ [ qw( textsize ) ] ],
+      Oracle => [ [ qw( long_read_len ) ] ],
     );
 
 my %ApacheSessionFlexParams =
